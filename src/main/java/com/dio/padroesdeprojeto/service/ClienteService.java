f38791498fcce292c5826b5aeec4b21d183167de
AgnoDev <agnodev87@gmail.com>
2023-05-17 10:36:17 -0300

    implementa service

---
+++ src/main/java/com/dio/padroesdeprojeto/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.dio.padroesdeprojeto.service;
+
+import com.dio.padroesdeprojeto.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
